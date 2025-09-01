2e54b70c85e7824a958c860b67c915c09dc18783
typescreep <typescreep@prorobotech.ru>
2025-09-01 17:51:20 +0300

    better clearify for form values

--- src/endpoints/forms/formSync/utils/removeAndRename.ts
+++ src/endpoints/forms/formSync/utils/removeAndRename.ts
@@ -23,43 +23,99 @@ import { TFormName } from 'src/localTypes/forms'
 //   return object
 // }
 
-export const removeEmptyFormValues = (
-  object: any,
-  persistedKeys: TFormName[],
-  currentPath: (string | number)[] = [],
-) => {
-  const isSamePath = (path1: (string | number)[], path2: (string | number)[]) => {
-    if (path1.length !== path2.length) return false
-    return path1.every((value, index) => value === path2[index])
+// ----
+
+// export const removeEmptyFormValues = (
+//   object: any,
+//   persistedKeys: TFormName[],
+//   currentPath: (string | number)[] = [],
+// ) => {
+//   const isSamePath = (path1: (string | number)[], path2: (string | number)[]) => {
+//     if (path1.length !== path2.length) return false
+//     return path1.every((value, index) => value === path2[index])
+//   }
+
+//   const isPathPersisted = (path: (string | number)[]) => {
+//     return persistedKeys.some(persistedPath =>
+//       isSamePath(path, Array.isArray(persistedPath) ? persistedPath : [persistedPath]),
+//     )
+//   }
+
+//   if (typeof object === 'object' && !Array.isArray(object) && object !== null) {
+//     Object.keys(object).forEach(key => {
+//       const newPath = [...currentPath, key]
+
+//       if (typeof object[key] === 'object' && !Array.isArray(object[key]) && object[key] !== null) {
+//         removeEmptyFormValues(object[key], persistedKeys, newPath)
+//         if (Object.keys(object[key]).length === 0 && !isPathPersisted(newPath)) {
+//           delete object[key]
+//         }
+//       } else {
+//         const isEmptyStringOrArray =
+//           (typeof object[key] === 'string' || Array.isArray(object[key])) && object[key].length === 0
+//         const isUndefined = object[key] === undefined
+
+//         if ((isEmptyStringOrArray || isUndefined) && !isPathPersisted(newPath)) {
+//           delete object[key]
+//         }
+//       }
+//     })
+//   }
+//   return object
+// }
+
+type PathSeg = string | number
+
+const OMIT: unique symbol = Symbol('omit')
+type MaybeOmit<T> = T | typeof OMIT
+
+const isSamePath = (a: PathSeg[], b: PathSeg[]) => a.length === b.length && a.every((v, i) => v === b[i])
+
+const isPathPersisted = (path: PathSeg[], persisted: TFormName[]) =>
+  persisted.some(p => isSamePath(path, Array.isArray(p) ? p : [p]))
+
+const clean = (value: any, persisted: TFormName[], path: PathSeg[]): MaybeOmit<any> => {
+  // Remove null/undefined unless persisted
+  if (value === null || value === undefined) {
+    return isPathPersisted(path, persisted) ? value : OMIT
   }
 
-  const isPathPersisted = (path: (string | number)[]) => {
-    return persistedKeys.some(persistedPath =>
-      isSamePath(path, Array.isArray(persistedPath) ? persistedPath : [persistedPath]),
-    )
+  // Primitives
+  if (typeof value !== 'object') {
+    if (typeof value === 'string' && value.length === 0) {
+      return isPathPersisted(path, persisted) ? value : OMIT
+    }
+    return value // number | boolean | non-empty string
   }
 
-  if (typeof object === 'object' && !Array.isArray(object) && object !== null) {
-    Object.keys(object).forEach(key => {
-      const newPath = [...currentPath, key]
+  // Arrays
+  if (Array.isArray(value)) {
+    const cleanedItems = value.map((item, idx) => clean(item, persisted, [...path, idx])).filter(item => item !== OMIT)
 
-      if (typeof object[key] === 'object' && !Array.isArray(object[key]) && object[key] !== null) {
-        removeEmptyFormValues(object[key], persistedKeys, newPath)
-        if (Object.keys(object[key]).length === 0 && !isPathPersisted(newPath)) {
-          delete object[key]
-        }
-      } else {
-        const isEmptyStringOrArray =
-          (typeof object[key] === 'string' || Array.isArray(object[key])) && object[key].length === 0
-        const isUndefined = object[key] === undefined
+    if (cleanedItems.length === 0) {
+      return isPathPersisted(path, persisted) ? [] : OMIT
+    }
+    return cleanedItems
+  }
 
-        if ((isEmptyStringOrArray || isUndefined) && !isPathPersisted(newPath)) {
-          delete object[key]
-        }
-      }
+  // Plain objects
+  const cleanedEntries = Object.entries(value)
+    .map(([k, v]) => {
+      const cleaned = clean(v, persisted, [...path, k])
+      return cleaned === OMIT ? null : ([k, cleaned] as const)
     })
+    .filter((e): e is readonly [string, any] => e !== null)
+
+  if (cleanedEntries.length === 0) {
+    return isPathPersisted(path, persisted) ? {} : OMIT
   }
-  return object
+
+  return Object.fromEntries(cleanedEntries)
+}
+
+export const removeEmptyFormValues = (input: any, persistedKeys: TFormName[]): any => {
+  const out = clean(input, persistedKeys, [])
+  return out === OMIT ? undefined : out
 }
 
 export const renameBrokenFieldBack = (object: any) => {
